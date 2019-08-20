c74fb776e7b17d0b8d4231219167885cc0313cf9
Lex Aeterna <lexaeterna12321@gmail.com>
2019-08-20 21:47:41 +0200

    changing format mechanism

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import Slider from "rc-slider";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { formatValue: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ formatValue: e.target.value }, () => {
+      this.props.changeFormat(this.state.formatValue);
+    });
+  }
   render() {
+    const { formatValue } = this.state;
     const { level, changeLevel } = this.props;
     return (
       <nav className="Navbar">
@@ -21,6 +34,13 @@ export class Navbar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={formatValue} onChange={this.handleChange}>
+            <MenuItem value={"hex"}>HEX - #ffffff</MenuItem>
+            <MenuItem value={"rgb"}>RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value={"rgba"}>RGBA - rgba(255,255,255,1)</MenuItem>
+          </Select>
         </div>
       </nav>
     );
