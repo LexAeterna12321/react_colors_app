ef3c83c94be503b51f1f55c483439843a6d36d86
Lex Aeterna <lexaeterna12321@gmail.com>
2019-08-21 21:49:46 +0200

    finished styling miniPalettes component

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,18 +1,54 @@
 import React, { Component } from "react";
 import MiniPalette from "./MiniPalette";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+  root: {
+    backgroundColor: "royalblue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center"
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap"
+  },
+  nav: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+    color: "white"
+  },
+  palettes: {
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3,30%)",
+    gridGap: "5%"
+  }
+};
 
 export class PaletteList extends Component {
   render() {
-    const { palettes } = this.props;
+    const { palettes, classes } = this.props;
     return (
-      <div>
-        <MiniPalette />
-        {palettes.map(palette => (
-          <MiniPalette {...palette} />
-        ))}
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Colors</h1>
+          </nav>
+          <div className={classes.palettes}>
+            {palettes.map(palette => (
+              <MiniPalette {...palette} key={palette.id} />
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
