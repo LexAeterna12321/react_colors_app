46d1ff40c3e5aa88119d4a64526758dda705ad23
Lex Aeterna <lexaeterna12321@gmail.com>
2019-08-26 20:46:25 +0200

    added basic side drawer component content

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -10,8 +10,9 @@ import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-
-const drawerWidth = 240;
+import Button from "@material-ui/core/Button";
+import { ChromePicker } from "react-color";
+const drawerWidth = 400;
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -120,6 +121,22 @@ const NewPaletteForm = () => {
           </IconButton>
         </div>
         <Divider />
+        <Typography variant="h4">Design Your palette</Typography>
+        <div>
+          <Button variant="contained" color="secondary">
+            Clear Palette
+          </Button>
+          <Button variant="contained" color="primary">
+            Random Color
+          </Button>
+        </div>
+        <ChromePicker
+          color="purple"
+          onChangeComplete={newColor => console.log(newColor)}
+        />
+        <Button variant="contained" color="primary">
+          Add Color
+        </Button>
       </Drawer>
       <main
         className={clsx(classes.content, {
