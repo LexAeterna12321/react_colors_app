2562ee9b6e91928c228d3acfc1ef93be47b61aa4
Lex Aeterna <lexaeterna12321@gmail.com>
2019-09-06 18:24:34 +0200

    Updates

---
+++ precache-manifest.9eb3fd4a1330dcdefad84ec0ed5ed8ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db271334c3153dec52c50a0884338292",
+    "url": "/react_colors_app/index.html"
+  },
+  {
+    "revision": "7142ac000bf1690ab200",
+    "url": "/react_colors_app/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "508d5364f4221bd9f1d2",
+    "url": "/react_colors_app/static/css/main.dd5d30ec.chunk.css"
+  },
+  {
+    "revision": "7142ac000bf1690ab200",
+    "url": "/react_colors_app/static/js/2.587b358c.chunk.js"
+  },
+  {
+    "revision": "508d5364f4221bd9f1d2",
+    "url": "/react_colors_app/static/js/main.a1d359e8.chunk.js"
+  },
+  {
+    "revision": "eb9a2231380050404782",
+    "url": "/react_colors_app/static/js/runtime~main.af3f579c.js"
+  },
+  {
+    "revision": "bb46f23f98d113fff6f20281b2b15362",
+    "url": "/react_colors_app/static/media/bg.bb46f23f.svg"
+  }
+]);
